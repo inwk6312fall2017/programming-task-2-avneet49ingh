d6e037a1f11b04c246073c6156e15fef486ae8e4
avneet singh <avneetsingh.yg30@gmail.com>
2017-10-02 15:46:27 +0000

    task

--- file1.py
+++ file1.py
@@ -1,1 +1,9 @@
-print("a")
+import csv
+class crime():
+  def func(self):
+    with open('Crime.csv', 'r') as myFile:
+    reader = csv.reader(myFile)
+    for line in reader:
+      t = line[7]
+      a = t.getdata
+        if  
